aa3fc4c3577b16e5e75740202fa0b420cdda784b
gagu1999 <48017554+gagu1999@users.noreply.github.com>
2020-09-22 23:55:31 +0900

    히히 파이썬 3에서 잘 돌아가도록 바꿨음
    random 모듈에 대해 많이 배웠땅

--- oop_test.py
+++ oop_test.py
@@ -32,23 +32,30 @@ else:
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())
 
-def convert(snippet, phrase):
+def convert(snippet, phrase): # snippet이랑 phrase 둘다 str임. 
     class_names = [w.capitalize() for w in 
                     random.sample(WORDS, snippet.count("%%%"))]
+    #random.sample이 리턴하는거 자체가 리스트긴한데, 클래스이름의 첫글자를 대문자로 만들어주려고 해서
+    #요롷게 쓰는거얌. 
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
     
     for i in range(0, snippet.count("@@@")):
         param_count = random.randint(1, 3)
-        param_names.append(', '.join(random.sample(WORDS, param_count)))
-        
+        param_names.append(b", ".join(random.sample(WORDS, param_count)))
+        '''
+        근데 왜 여기서 1~3사이의 수를 랜덤으로 뽑는지 모르겠네.
+        아 이해했따. @@@이 꼭 변수 1개를 의미하는게 아니라 
+        def main(x, y, z) 처럼 최대 3개까지 변수가 들어가는 걸 표현하려고 이렇게 만든거네 ㅋㅋ.
+        '''
     for sentence in snippet, phrase:
         result = sentence[:]
         
         #가짜 클래스 이름
         for word in class_names:
             result = result.replace('%%%', word.decode('utf-8'), 1)
+            # %%%를 word로 1번 바꾼다는 소리.
         
         #가짜 나머지 이름
         for word in other_names:
@@ -59,7 +66,9 @@ def convert(snippet, phrase):
             result = result.replace("@@@", word.decode('utf-8'), 1)
             
         results.append(result)
-    return results
+    # snippet에 대해서 한번, pharse에 대해서 한번 총 2번 for문을 돌고
+    # 각각을 변환한 문장을 results에 저장함. 
+    return results # results = [snippet 변환한 문장, phrase 변환한 문장]이 되는거지..
     
 #CTRL-D를 누를 때까지 계속한다
 try:
